20cfe3065b93a4a8b52c661af02c372dbbbda0a4
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-06 18:31:18 +0300

    Update StudentImpl.java

--- Server/StudentImpl.java
+++ Server/StudentImpl.java
@@ -1,6 +1,10 @@
 import java.rmi.*;
 
 public class StudentImpl implements Student{
+
+    public void getName() throws RemoteException{
+        System.out.println("Student Name");
+    }
     public static void main(String[] args) throws Exception{
         
     }
