a5c98f9110e5cb251b033d5ab5b12ebc888343e5
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-06 19:11:04 +0300

    more updates

--- Client/Client.java
+++ Client/Client.java
@@ -7,6 +7,6 @@ public class Client {
     public static void main(String[] args) throws Exception{
         Registry registry = LocateRegistry.getRegistry("127.0.0.1", 9000);
         Student student = (Student)registry.lookup("l");
-        student.getName();
+        System.out.println(student.getName());
     }
 }
