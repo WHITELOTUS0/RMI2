9d58ab196bd8489cbd6fc498c6afa0ef26f285c4
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-06 18:36:45 +0300

    Update Server.java

--- Server/Server.java
+++ Server/Server.java
@@ -3,6 +3,6 @@ import java.rmi.registry.*;
 
 public class Server {
     public static void main(String[] args) throws Exception{
-        
+        System.setProperty("java.rmi.server.hostname", "127.0.0.1");
     }
 }
