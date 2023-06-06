7a19d17898099048ac21769b5131ec83db149f38
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2023-06-06 18:43:39 +0300

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -6,5 +6,6 @@ import java.rmi.registry.Registry;
 public class Client {
     public static void main(String[] args) throws Exception{
         Registry registry = LocateRegistry.getRegistry("127.0.0.1", 9000);
+        Student student = (Student)registry.lookup("l");
     }
 }
